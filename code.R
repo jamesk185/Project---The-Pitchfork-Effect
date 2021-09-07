39c5dc9cfff33d3b5fd906f09871e2cc49fb8a4a
James Kowalik <jameskowalik@outlook.com>
2021-09-07 18:59:05 +0900

    2nd function, prevbnmtotal, completed and added

--- code.R
+++ code.R
@@ -9,10 +9,12 @@ p4kdata <- read.csv("./pitchfork.csv")
 p4kdata <- p4kdata %>% select(-link, -review)
 p4kdata$artist <- as.character(p4kdata$artist)
 p4kdata$date <- as.Date(p4kdata$date, format = "%B %d %Y")
+p4kdata <- p4kdata[order(desc(p4kdata$date)),]
+
 
 artistnames <- unique(p4kdata$artist)
 
-PrevTotal <- function(name){
+PrevTotalfn <- function(name){
   artist <<- p4kdata[p4kdata$artist==name,]
   y <<- NULL
   for(x in 1:nrow(artist)){
@@ -21,9 +23,26 @@ PrevTotal <- function(name){
   }
   oneartist <<- cbind(artist, PrevTotal = y)
 }
-p4kdata2 <- lapply(artistnames, PrevTotal)
-p4kdata2 <- bind_rows(p4kdata2)
 
+p4kdata2 <- lapply(artistnames, PrevTotalfn)
+p4kdata2 <- bind_rows(p4kdata2)
+head(p4kdata2, n=30)
+
+
+PrevBNMTotalfn <- function(name){
+  artist <<- p4kdata2[p4kdata2$artist==name,]
+  y <<- NULL
+  for(x in 1:nrow(artist)){
+    a <- artist[artist$date < artist$date[x],]
+    b <- sum(a$bnm)
+    y <- c(y,b)
+  }
+  oneartist <<- cbind(artist, PrevBNMTotal = y)
+}
+
+p4kdata3 <- lapply(artistnames, PrevBNMTotalfn)
+p4kdata3 <- bind_rows(p4kdata3)
+head(p4kdata3, n=30)
 
 
 
