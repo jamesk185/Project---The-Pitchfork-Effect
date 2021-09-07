7d6c900bfa70567d2f7ed4b30f1e90498b8795b1
James Kowalik <jameskowalik@outlook.com>
2021-09-07 17:55:28 +0900

    first function, PrevTotal, finished

--- code.R
+++ code.R
@@ -5,14 +5,13 @@ library(tidyverse)
 URL <- "https://dl.dropboxusercontent.com/s/cqf7cgxh91eoeyn/pitchfork.csv"
 if(!file.exists("./pitchfork.csv")){download.file(URL, "./pitchfork.csv")}
 p4kdata <- read.csv("./pitchfork.csv")
+
 p4kdata <- p4kdata %>% select(-link, -review)
 p4kdata$artist <- as.character(p4kdata$artist)
 p4kdata$date <- as.Date(p4kdata$date, format = "%B %d %Y")
 
 artistnames <- unique(p4kdata$artist)
 
-
-p4kdata2 <- data.frame(NULL)
 PrevTotal <- function(name){
   artist <<- p4kdata[p4kdata$artist==name,]
   y <<- NULL
@@ -21,26 +20,9 @@ PrevTotal <- function(name){
     y <- c(y,z)
   }
   oneartist <<- cbind(artist, PrevTotal = y)
-  p4kdata2 <<- rbind(p4kdata2, oneartist)
 }
-
-
-
-
-
-PrevTotal("Andrew Bird")
-p4kdata2
-
-PrevTotal("Aphex Twin")
-p4kdata2
-
-sapply(artistnames, PrevTotal)
-p4kdata2
-
-with(artistnames, PrevTotal())
-
-
-
+p4kdata2 <- lapply(artistnames, PrevTotal)
+p4kdata2 <- bind_rows(p4kdata2)
 
 
 
